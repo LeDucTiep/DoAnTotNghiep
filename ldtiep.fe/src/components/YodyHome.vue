904b9a31c26ecdd89fa24a53b68c5fb6c75a162e
LeDucTiep <tieple1782002@gmail.com>
2024-10-27 22:08:12 +0700

    Thêm gợi ý sản phẩm

--- ldtiep.fe/src/components/YodyHome.vue
+++ ldtiep.fe/src/components/YodyHome.vue
@@ -30,7 +30,7 @@
         ></span>
 
         <div class="header__menu d-flex">
-          <Popover :hover="true" :arrow="true">
+          <Popover :arrow="true">
             <div class="menu-item">BST Thu Đông</div>
             <template #content>
               <div class="menu-popover">
@@ -226,6 +226,76 @@
           <div class="button-view-more">Xem thêm</div>
         </div>
       </div>
+
+      <div class="goi-y">
+        <div class="goi-y-title">
+          <div class="left">Gợi ý sản phẩm</div>
+          <div class="right">
+            <Popover
+              @open:popover="isShowGoiY = true"
+              @close:popover="isShowGoiY = false"
+            >
+              <div class="goi-y-show d-flex">
+                <div class="goi-y-right">{{ GoiYs[currGoiYId] }}</div>
+
+                <div class="icon-right">
+                  <svg
+                    v-show="!isShowGoiY"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12 16.8C11.3 16.8 10.6 16.53 10.07 16L3.55002 9.48001C3.26002 9.19001 3.26002 8.71001 3.55002 8.42001C3.84002 8.13001 4.32002 8.13001 4.61002 8.42001L11.13 14.94C11.61 15.42 12.39 15.42 12.87 14.94L19.39 8.42001C19.68 8.13001 20.16 8.13001 20.45 8.42001C20.74 8.71001 20.74 9.19001 20.45 9.48001L13.93 16C13.4 16.53 12.7 16.8 12 16.8Z"
+                      fill="currentcolor"
+                    ></path>
+                  </svg>
+                  <svg
+                    v-show="isShowGoiY"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    id=":rdp:"
+                    class="text-black"
+                  >
+                    <path
+                      d="M19.92 15.8C19.73 15.8 19.54 15.73 19.39 15.58L12.87 9.05996C12.39 8.57996 11.61 8.57996 11.13 9.05996L4.61002 15.58C4.32002 15.87 3.84002 15.87 3.55002 15.58C3.26002 15.29 3.26002 14.81 3.55002 14.52L10.07 7.99996C11.13 6.93996 12.86 6.93996 13.93 7.99996L20.45 14.52C20.74 14.81 20.74 15.29 20.45 15.58C20.3 15.72 20.11 15.8 19.92 15.8Z"
+                      fill="currentcolor"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+              <template #content>
+                <div class="goi-y-menu">
+                  <div
+                    v-for="(item, index) in GoiYs"
+                    :key="index"
+                    class="goi-y-item"
+                    :class="{ 'goi-y-item--active': currGoiYId == index }"
+                    @click="changeGoiY(index)"
+                  >
+                    {{ item }}
+                  </div>
+                </div>
+              </template>
+            </Popover>
+          </div>
+        </div>
+        <div class="products-block">
+          <div class="product-row">
+            <ProductSP></ProductSP>
+            <ProductSP></ProductSP>
+          </div>
+          <div class="product-row">
+            <ProductSP></ProductSP>
+            <ProductSP></ProductSP>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="home-footer"></div>
@@ -250,10 +320,16 @@ export default {
     return {
       inputValue: "",
       currUaChuongId: 0,
+      currGoiYId: 0,
       swiper: null,
+      GoiYs: ["Áo Giữ Nhiệt", "Áo Thun", "Áo Polo", "Đồ Lót"],
+      isShowGoiY: false,
     };
   },
   methods: {
+    changeGoiY(i) {
+      this.currGoiYId = i;
+    },
     changeUaChuong(i) {
       this.currUaChuongId = i;
     },
