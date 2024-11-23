d53fc8b14fc333d1caa54f1d9e2fc1beed33d64f
LeDucTiep <tieple1782002@gmail.com>
2024-11-23 19:21:19 +0700

    Hiển thị danh sách sản phẩm trên giỏ hàng

--- ldtiep.fe/src/components/product-cart/ProductCart.vue
+++ ldtiep.fe/src/components/product-cart/ProductCart.vue
@@ -2,118 +2,153 @@
   <div class="product-cart">
     <div class="product-cart-header">Giỏ hàng</div>
 
-    <div class="product-list-header">
-      <div class="select-all-box">
-        <TCheckbox v-model="SelectedAll" @change="onSelectAll()"> </TCheckbox>
-      </div>
-      <div class="select-all-name">Đã chọn 0 sản phẩm</div>
-    </div>
-    <div class="product-list">
-      <div
-        v-for="(item, index) in Products"
-        :key="index"
-        class="product-container-cart"
-      >
-        <TCheckbox v-model="SelecteCell[index]" @change="onSelectChange()">
-        </TCheckbox>
+    <div class="product-cart-body">
+      <div class="product-cart--left">
+        <div class="product-list-header">
+          <div class="select-all-box">
+            <TCheckbox v-model="SelectedAll" @change="onSelectAll()">
+            </TCheckbox>
+          </div>
+          <div class="select-all-name">Đã chọn 0 sản phẩm</div>
+        </div>
+        <div class="product-list">
+          <div
+            v-for="(item, index) in Products"
+            :key="index"
+            class="product-container-cart"
+          >
+            <TCheckbox v-model="SelecteCell[index]" @change="onSelectChange()">
+            </TCheckbox>
 
-        <div class="product-container-cart--right d-flex">
-          <img
-            loading="lazy"
-            decoding="async"
-            height="160"
-            src="https://localhost:44356/api/v1/Pictures/d886ad44-0002-4eaa-bd50-c00d1c089369"
-          />
+            <div class="product-container-cart--right d-flex">
+              <img
+                loading="lazy"
+                decoding="async"
+                height="160"
+                :src="PictureApi.baseUrl + '/' + item.PictureID"
+              />
 
-          <div class="cart-item-right">
-            <div class="cart-item-right--top">
-              <div class="name">{{ item.ProductName }}</div>
-              <div class="pri d-flex">
-                <div class="price">
-                  {{ format(item.Price) }}
+              <div class="cart-item-right">
+                <div class="cart-item-right--top">
+                  <div class="name">{{ item.ProductName }}</div>
+                  <div class="pri d-flex">
+                    <div class="price">
+                      {{ format(item.Price) }}
+                    </div>
+                    <div class="org-price">
+                      {{ format(item.OriginalPrice) }}
+                    </div>
+                  </div>
+
+                  <div
+                    v-if="item.Discount"
+                    class="w-fit inline-flex items-center h-4 border-y-[0.6px] border-yd-alert-error bg-yd-alert-error-background text-yd-alert-error text-yd-subtitle-4"
+                  >
+                    <svg
+                      width="4"
+                      height="15"
+                      viewBox="0 0 4 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        y="0.600006"
+                        width="0.6"
+                        height="4"
+                        fill="#E14337"
+                      ></rect>
+                      <path
+                        d="M0 4.60001C1.65685 4.60001 3 5.94315 3 7.60001C3 9.25686 1.65685 10.6 0 10.6V4.60001Z"
+                        fill="white"
+                      ></path>
+                      <path
+                        d="M0 4.60001C1.65685 4.60001 3 5.94315 3 7.60001C3 9.25686 1.65685 10.6 0 10.6"
+                        stroke="#E14337"
+                        stroke-width="0.6"
+                      ></path>
+                      <rect
+                        y="10.6"
+                        width="0.6"
+                        height="4"
+                        fill="#E14337"
+                      ></rect></svg
+                    ><span class="px-1"
+                      >Đã tiết kiệm -{{
+                        format(item.OriginalPrice - item.Price)
+                      }}</span
+                    ><svg
+                      width="4"
+                      height="15"
+                      viewBox="0 0 4 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        x="3.40039"
+                        y="0.599998"
+                        width="0.6"
+                        height="4"
+                        fill="#E14337"
+                      ></rect>
+                      <path
+                        d="M4 10.6C2.34315 10.6 1 9.25685 1 7.6C1 5.94314 2.34315 4.6 4 4.6L4 10.6Z"
+                        fill="white"
+                      ></path>
+                      <path
+                        d="M4 10.6C2.34315 10.6 1 9.25685 1 7.6C1 5.94314 2.34315 4.6 4 4.6"
+                        stroke="#E14337"
+                        stroke-width="0.6"
+                      ></path>
+                      <rect
+                        x="3.40039"
+                        y="10.6"
+                        width="0.6"
+                        height="4"
+                        fill="#E14337"
+                      ></rect>
+                    </svg>
+                  </div>
                 </div>
-                <div class="org-price">
-                  {{ format(item.OriginalPrice) }}
+
+                <div class="cart-item-right--bottom">
+                  <div class="color-and-size">
+                    {{ item.ColorName }}, {{ item.SizeName }}
+                  </div>
+                  <InputCounter v-model="item.ProductCartCount"></InputCounter>
                 </div>
               </div>
-
-              <div
-                v-if="item.Discount"
-                class="w-fit inline-flex items-center h-4 border-y-[0.6px] border-yd-alert-error bg-yd-alert-error-background text-yd-alert-error text-yd-subtitle-4"
-              >
-                <svg
-                  width="4"
-                  height="15"
-                  viewBox="0 0 4 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    y="0.600006"
-                    width="0.6"
-                    height="4"
-                    fill="#E14337"
-                  ></rect>
-                  <path
-                    d="M0 4.60001C1.65685 4.60001 3 5.94315 3 7.60001C3 9.25686 1.65685 10.6 0 10.6V4.60001Z"
-                    fill="white"
-                  ></path>
-                  <path
-                    d="M0 4.60001C1.65685 4.60001 3 5.94315 3 7.60001C3 9.25686 1.65685 10.6 0 10.6"
-                    stroke="#E14337"
-                    stroke-width="0.6"
-                  ></path>
-                  <rect
-                    y="10.6"
-                    width="0.6"
-                    height="4"
-                    fill="#E14337"
-                  ></rect></svg
-                ><span class="px-1"
-                  >Đã tiết kiệm -{{
-                    format(item.OriginalPrice - item.Price)
-                  }}</span
-                ><svg
-                  width="4"
-                  height="15"
-                  viewBox="0 0 4 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    x="3.40039"
-                    y="0.599998"
-                    width="0.6"
-                    height="4"
-                    fill="#E14337"
-                  ></rect>
-                  <path
-                    d="M4 10.6C2.34315 10.6 1 9.25685 1 7.6C1 5.94314 2.34315 4.6 4 4.6L4 10.6Z"
-                    fill="white"
-                  ></path>
-                  <path
-                    d="M4 10.6C2.34315 10.6 1 9.25685 1 7.6C1 5.94314 2.34315 4.6 4 4.6"
-                    stroke="#E14337"
-                    stroke-width="0.6"
-                  ></path>
-                  <rect
-                    x="3.40039"
-                    y="10.6"
-                    width="0.6"
-                    height="4"
-                    fill="#E14337"
-                  ></rect>
-                </svg>
-              </div>
-            </div>
-
-            <div class="cart-item-right--bottom">
-              <div class="color-and-size">
-                {{ item.ColorName }}, {{ item.SizeName }}
-              </div>
-              <InputCounter v-model="item.ProductCartCount"></InputCounter>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="product-cart--right">
+        <div class="title">Chi tiết đơn hàng</div>
+        <div class="infor-cart">
+          <div class="row d-flex">
+            <div class="name">Tổng giá trị sản phẩm</div>
+            <div class="value">160 d</div>
+          </div>
+          <div class="row d-flex">
+            <div class="name">Vận chuyển</div>
+            <div class="value">160 d</div>
+          </div>
+          <div v-if="true" class="row d-flex">
+            <div class="name">Giảm giá vận chuyển</div>
+            <div class="value tran-cost">-160 d</div>
+          </div>
+          <div class="row-hr"></div>
+          <div class="row d-flex total-cost-row">
+            <div class="name">Tổng thanh toán</div>
+            <div class="value">160 d</div>
+          </div>
+
+          <div class="row d-flex total-extra-row">
+            <div class="name">Bạn đã tiết kiệm được 300.000 đ</div>
+          </div>
+        </div>
+
+        <div class="by-product-button">
+          <div class="by-product">Mua hàng</div>
         </div>
       </div>
     </div>
@@ -155,6 +190,7 @@ export default {
       e.Price = p.Price;
       e.Discount = p.Discount;
       e.OriginalPrice = p.OriginalPrice;
+      e.PictureID = p.PictureIDS.split(";")[0];
     }
 
     console.log(this.Products);
